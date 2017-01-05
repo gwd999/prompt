532766720b99d2ff9fe677e81f539e9adc59d1c4
Henrik Bengtsson <hb@aroma-project.org>
2017-01-05 14:48:50 -0800

    Add suspend(), restore() and toggle() (Issue #3) (#4)

--- R/package.R
+++ R/package.R
@@ -11,6 +11,9 @@ prompt_env <- new.env()
 prompt_env$prompt <- "> "
 prompt_env$task_id <- NA
 prompt_env$error <- NULL
+prompt_env$default_prompt <- prompt_env$prompt
+prompt_env$disabled_prompt <- NULL
+prompt_env$in_use <- TRUE
 
 .onLoad <- function(libname, pkgname) {
   assign("task_id", addTaskCallback(update_callback), envir = prompt_env)
@@ -37,12 +40,18 @@ update_prompt <- function(...) {
   if (is.string(mine)) options(prompt = mine)
 }
 
-#' Set the prompt
+#' Set and control the prompt
 #'
 #' @param value A character string for a static prompt, or
 #'   a function that is called after the evaluation every expression
 #'   typed at the R prompt. The function should always return a
 #'   character scalar.
+#'
+#' @details
+#' Function \code{update_prompt()} is used to replace the default \R
+#' prompt with a custom prompt.   A custom prompt can be disabled
+#' with \code{suspend()} and then re-enable with \code{restore()}.
+#' Function \code{toggle()} toggles between the two.
 #'
 #' @export
 
@@ -50,4 +59,33 @@ set_prompt <- function(value) {
   stopifnot(is.function(value) || is.string(value))
   assign("prompt", value, envir = prompt_env)
   update_prompt(NULL, NULL, TRUE, FALSE)
+}
+
+
+#' @rdname set_prompt
+#' @export
+
+suspend <- function() {
+  if (!prompt_env$in_use) return(invisible(FALSE))
+  prompt_env$disabled_prompt <- prompt_env$prompt
+  set_prompt(prompt_env$default_prompt)
+  prompt_env$in_use <- FALSE
+  invisible(TRUE)
+}
+
+#' @rdname set_prompt
+#' @export
+
+restore <- function() {
+  if (prompt_env$in_use) return(invisible(FALSE))
+  set_prompt(prompt_env$disabled_prompt)
+  prompt_env$in_use <- TRUE
+  invisible(TRUE)
+}
+
+#' @rdname set_prompt
+#' @export
+
+toggle <- function() {
+  if (prompt_env$in_use) suspend() else restore()
 }
