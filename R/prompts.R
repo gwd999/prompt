4c43189ff2cff25616c460c2680ba3458d29ef8c
Gábor Csárdi <csardi.gabor@gmail.com>
2021-02-26 10:48:18 +0100

    Remove the running time prompt
    It does not work well.

--- R/prompts.R
+++ R/prompts.R
@@ -1,30 +1,4 @@
 
-prompt_runtime_factory <- function() {
-
-  last <- proc.time()
-  last[] <- NA_real_
-
-  function(...) {
-    diff <- proc.time() - last
-    elapsed <- sum(diff) - diff["elapsed"]
-    last <<- proc.time()
-    if (!is.na(elapsed) && elapsed > 1) {
-      paste0(round(elapsed), "s > ")
-    } else {
-      "> "
-    }
-  }
-}
-
-#' A prompt that shows the CPU time used by the last top level expression
-#'
-#' @param ... Arguments, ignored.
-#'
-#' @family example prompts
-#' @export
-
-prompt_runtime <- prompt_runtime_factory()
-
 #' A prompt that shows the status (OK or error) of the last expression
 #'
 #' @param expr Evaluated expression.
